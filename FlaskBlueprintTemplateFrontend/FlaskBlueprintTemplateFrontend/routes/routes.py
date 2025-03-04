a59f169a993089dea8b27b0683280321a117a4cd
Liam Schott <liam.schott@dwarfanimation.com>
2025-03-04 11:54:07 +0100

    Flask run working for Api

--- FlaskBlueprintTemplateFrontend/FlaskBlueprintTemplateFrontend/routes/routes.py
+++ FlaskBlueprintTemplateFrontend/FlaskBlueprintTemplateFrontend/routes/routes.py
@@ -5,6 +5,11 @@ This is a placeholder/example of routes/views for our api blueprint.
 All our Views comes here or could be seperated into several files
 
 """
+from FlaskBlueprintTemplateFrontend.extensions import (
+    Blueprint,
+    render_template
+)
+
 routes_bp = Blueprint('routes', __name__)  # Define the blueprint
 
 @routes_bp.route('/')
